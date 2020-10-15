bdf7795bc266d2b8dcc8a253fea8279c0c4d3151
JMMarland <unconfigured@null.spigotmc.org>
2020-10-15 23:11:21 +0100

    Started implementation

---
+++ BinarySearchTree/BinarySearchTree/BinSearchTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BinarySearchTree
+{
+    public class BinSearchTree<KEY, VALUE> where KEY : IComparable
+    {
+        private TreeNode<KEY, VALUE> _root = null;
+
+        public void Insert(KEY key, VALUE value)
+        {
+            if (_root == null)
+            {
+                _root = new TreeNode<KEY, VALUE>(key, value); ;
+                return;
+            }
+
+            TreeNode<KEY, VALUE > currentNode = _root;
+            bool isSearching = true;
+
+            while (isSearching)
+            {
+                int comparison = currentNode.Key.CompareTo(key);
+
+                if (comparison == 0)
+                {
+                    currentNode.Value = value;
+                    return;
+                }
+
+                if (comparison > 0)
+                {
+
+                }
+            }
+        }
+    }
+}
---
+++ BinarySearchTree/BinarySearchTree/TreeNode.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinarySearchTree
+{
+    internal class TreeNode<KEY, VALUE>
+    {
+        public KEY Key => _key;
+        private KEY _key;
+        public VALUE Value;
+        
+        public TreeNode<KEY, VALUE> Parent = null;
+        public TreeNode<KEY, VALUE> LeftChild = null;
+        public TreeNode<KEY, VALUE> RightChild = null;
+
+        public TreeNode(KEY key, VALUE value)
+        {
+            _key = key;
+            Value = value;
+        }
+    }
+}
